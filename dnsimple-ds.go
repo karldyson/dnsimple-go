6a5742ae4d82b6b2b6cdf8414c5717e21c1d4d6b
Karl Dyson <code@karld.uk>
2024-03-27 16:41:32 +0000

    split common functions out into separate file in order to re-use them in future project code. added a Makefile to ease cross compiling for linux

--- dnsimple-ds.go
+++ dnsimple-ds.go
@@ -16,32 +16,15 @@ TODO (no particular order):
 */
 
 import (
-	"bufio"
 	"context"
-	"errors"
 	"flag"
 	"fmt"
-	"net"
-	"net/http"
 	"os"
-	"runtime"
+	"runtime/debug"
 	"strconv"
-	"strings"
 
-	"github.com/bigkevmcd/go-configparser"
 	"github.com/dnsimple/dnsimple-go/dnsimple"
 	"github.com/miekg/dns"
-)
-
-// global variable declarations
-var (
-	configFile = flag.String("config", "/usr/local/etc/dnsimple.cfg", "configuration file") // the configuration file
-	debug      = flag.Bool("debug", false, "enable debug output")                           // debug?
-	verbose    = flag.Bool("verbose", false, "verbose output")                              // verbosity?
-	config     map[string]string                                                            // the configuration map
-	cp         *configparser.ConfigParser                                                   // pointer to the config parser object although I think this is uneeded?
-	tc         *http.Client                                                                 // pointer to the global token client object
-	apiClient  *dnsimple.Client                                                             // pointer to the global API client object
 )
 
 // main collects the CLI flags,
@@ -60,8 +43,18 @@ func main() {
 	flag.Parse()
 
 	// set verbosity if debug is enabled
-	if *debug && !*verbose {
+	if *debugOutput && !*verbose {
 		*verbose = true
+	}
+
+	if *version {
+		//		goVer := runtime.Version
+		bi, ok := debug.ReadBuildInfo()
+		if !ok {
+			panic("not ok!")
+		}
+		fmt.Printf("%s version information:\n%+v\n", os.Args[0], bi)
+		return
 	}
 
 	config = make(map[string]string)
@@ -220,337 +213,5 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: unknown action: %s\n", action)
 		flag.Usage()
 		os.Exit(1)
-	}
-}
-
-// askUserYesNo takes a string and prompts the user with that string and a y/N
-// If the user replies with y, Y, yes, Yes, then it returns true
-// Anything else returns false
-// It takes one parameter, the string to be prompted to the user
-// It returns one bool depending on whether the user said Y or not.
-func askUserYesNo(s string) bool {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("%s [y/N]: ", s)
-	response, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: error requesting confirmation from user: %s\n", err)
-		os.Exit(1)
-	}
-	response = strings.ToLower(strings.TrimSpace(response))
-	if response == "y" || response == "yes" {
-		return true
-	} else {
-		// you can
-		// else if response == "n" || response == "no" {
-		// and wrap in a for {} to force y or n
-		return false
-	}
-}
-
-// doQuery performs DNS lookups. It takes two parameters, the domain to be looked up and the qtype
-// queries are performed over TCP, with DO and RD set
-// queries are sent to the nameserver and port parsed from the config
-// It returns the dns response object amd an error object
-func doQuery(qname string, qtype uint16) (*dns.Msg, error) {
-
-	nameserverAddr := config["nameserverAddr"]
-	nameserverPort := config["nameserverPort"]
-
-	_debug(fmt.Sprintf("Sending query for %s/%s to %s", qname, dns.TypeToString[qtype], net.JoinHostPort(nameserverAddr, nameserverPort)))
-
-	c := new(dns.Client)
-	c.Net = "tcp"
-	m := new(dns.Msg)
-	m.RecursionDesired = true
-	m.SetEdns0(4096, true) // do DNSKEY (set DO, as we want AD)
-	m.SetQuestion(dns.Fqdn(qname), qtype)
-	r, rtt, err := c.Exchange(m, net.JoinHostPort(nameserverAddr, nameserverPort))
-	_verbose(fmt.Sprintf("Response received from %s:%s in %s\n", nameserverAddr, nameserverPort, rtt))
-	if err != nil {
-		_debug(fmt.Sprintf("Error: query for %s/%s resulted in error: %s", qname, dns.TypeToString[qtype], err))
-		return nil, err
-	}
-	if r == nil || r.Rcode == dns.RcodeNameError || r.Rcode == dns.RcodeSuccess {
-		_debug(fmt.Sprintf("query for %s/%s resulted in rcode %s", qname, dns.TypeToString[qtype], dns.RcodeToString[r.Rcode]))
-		return r, err
-	}
-	return nil, errors.New("wibble")
-}
-
-// getApiClient either creates or passes back an existing globel client object
-// it takes no parameters
-// it returns a pointer to the client object
-func getApiClient() *dnsimple.Client {
-	// feels like there's not a lot of error catching/handling going on here...
-	if tc == nil {
-		_debug("token client does not exist, so creating it")
-		tc = dnsimple.StaticTokenHTTPClient(context.Background(), config["apiKey"])
-	}
-	if apiClient == nil {
-		_debug("api client doesn't exist, so creating it")
-		apiClient = dnsimple.NewClient(tc)
-	}
-	if apiClient != nil && config["apiEndpoint"] != "" {
-		_debug(fmt.Sprintf("setting API endpoint to %s", config["apiEndpoint"]))
-		//		apiClient.BaseURL = "https://api.sandbox.dnsimple.com"
-		apiClient.BaseURL = config["apiEndpoint"]
-	}
-	return apiClient
-}
-
-// getDsFromRegistry uses the registrar API to get a list of the DS records in the registry
-// It takes one parameter, the domain to be queried
-// It returns two parameters, the DS record response and an error object
-func getDsFromRegistry(domain string) (*dnsimple.DelegationSignerRecordsResponse, error) {
-	client := getApiClient()
-	dsResponse, err := client.Domains.ListDelegationSignerRecords(context.Background(), config["accountNumber"], domain, nil)
-	if err != nil {
-		errorString := fmt.Sprintf("Error: error fetching DS records from registry for domain %s: %s\n", domain, err)
-		return nil, errors.New(errorString)
-	}
-	return dsResponse, nil
-}
-
-// listDsInRegistry produces pretty (!!) output of DS records found in the registry
-// It takes one parameter, that being the domain to be queried
-// It returns nothing.
-func listDsInRegistry(domain string) {
-	dsRecords, err := getDsFromRegistry(domain)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: error fetching DS records from the registry: %s\n", err)
-		os.Exit(1)
-	}
-	switch len(dsRecords.Data) {
-	case 1:
-		fmt.Printf("There is %d DS record\n", len(dsRecords.Data))
-	default:
-		fmt.Printf("There are %d DS records\n", len(dsRecords.Data))
-	}
-	for _, ds := range dsRecords.Data {
-		fmt.Printf("  => DS %s %s %s %s\n", ds.Keytag, ds.Algorithm, ds.DigestType, ds.Digest)
-	}
-}
-
-// dsExistsInRegistry determines whether a specific DS record exists in the registry
-// It takes two parameters, the domain to be queried and the keytag to be verified
-// It returns a DS object, an error object and a boolean
-// The error will contain any errors encountered
-// The boolean determines whether the DS record exists
-// Hence, the combination determines the status:
-// error == nil, ok bool is true => DS exists (DS will be in the DS object)
-// error != nil, ok bool is true => DS does not exist
-// error != nil, ok bool is false => an error occurred trying
-func dsExistsInRegistry(domain string, keytag uint16) (dnsimple.DelegationSignerRecord, error, bool) {
-	dsRecords, err := getDsFromRegistry(domain)
-	var dsr dnsimple.DelegationSignerRecord
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: error retrieving list of keys for %s: %s\n", domain, err)
-		return dsr, errors.New(fmt.Sprintf("error retrieving list of keys: %s", err)), false
-	} else {
-		for _, ds := range dsRecords.Data {
-			_debug(fmt.Sprintf("got ds with keytag %s", ds.Keytag))
-			dsKeytag, err := strconv.ParseUint(ds.Keytag, 10, 16)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error: error converting keytag: %s", err)
-				os.Exit(1)
-			}
-			if uint16(dsKeytag) == keytag {
-				_debug(fmt.Sprintf("DS with keytag %d exists in the registry\n", keytag))
-				return ds, nil, true
-			}
-		}
-		return dsr, errors.New("DS record does not exist"), true
-	}
-
-}
-
-// getDnskeyFromDns looks up DNSKEY records in DNS
-// It takes one parameter, the domain to be queried
-// It returns a map of DNSKEYs indexed by keytag and an error object
-// It expects the query to be done with RD and DO and that the response returned to it
-// is either authoritative (AA) or validated (AD) - you should trust the validator if the latter!
-func getDnskeyFromDns(qname string) (map[uint16]dns.DNSKEY, error) {
-
-	r, err := doQuery(qname, dns.TypeDNSKEY)
-	// duplication of error checking from the doQuery function...
-	if err != nil || r == nil {
-		fmt.Fprintf(os.Stderr, "Cannot retrieve keys for %s: %s\n", qname, err)
-		os.Exit(1)
-	}
-	if r.Rcode == dns.RcodeNameError {
-		fmt.Fprintf(os.Stderr, "No such domain %s\n", qname)
-		os.Exit(1)
-	}
-
-	if r.Rcode != dns.RcodeSuccess {
-		fmt.Printf("Error in query for %s/DNSKEY: %s", qname, dns.RcodeToString[r.Rcode])
-		os.Exit(1)
-	}
-	if !(r.Authoritative || r.AuthenticatedData) {
-		fmt.Fprintf(os.Stderr, "response is neither authoritative nor validated")
-		os.Exit(1)
-	}
-
-	// we should (prep, and) return a map of resource record objects instead of this.
-	dnskeys := make(map[uint16]dns.DNSKEY)
-	for _, ans := range r.Answer {
-		switch rr := ans.(type) {
-		case *dns.DNSKEY:
-			_debug(fmt.Sprintf("got DNSKEY with keytag %d and flags %d", rr.KeyTag(), rr.Flags))
-			dnskeys[rr.KeyTag()] = *rr
-		}
-	}
-	if len(dnskeys) > 0 {
-		return dnskeys, nil
-	} else {
-		return nil, errors.New("no keys found in DNS")
-	}
-}
-
-// listDnsketInDns produces pretty output of the DNSKEY records found in DNS
-// It takes one parameter, that being the domain to be queried
-// It returns nothing.
-func listDnskeyInDns(domain string) {
-	dnskeys, err := getDnskeyFromDns(domain)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: error fetching DNSKEYs from DNS for domain %s: %s\n", domain, err)
-		os.Exit(1)
-	}
-	switch len(dnskeys) {
-	case 1:
-		fmt.Printf("There is %d DNSKEY record\n", len(dnskeys))
-	default:
-		fmt.Printf("There are %d DNSKEY records\n", len(dnskeys))
-	}
-	for key := range dnskeys {
-		k := dnskeys[key]
-		var keyType string
-		switch k.Flags {
-		case 256:
-			keyType = "ZSK"
-		case 257:
-			keyType = "KSK"
-		default:
-			fmt.Fprintf(os.Stderr, "Error: DNSKEY keytag %d in domain %s has unknown flags: %d\n", key, domain, k.Flags)
-			os.Exit(1)
-		}
-		fmt.Printf("  => DNSKEY; keytag: %5d; flags: %d (%s); Algorithm: %d (%s)\n", key, k.Flags, keyType, k.Algorithm, dns.AlgorithmToString[k.Algorithm])
-	}
-}
-
-// dnskeyExistsInDns determinies whether a DNSKEY exists in DNS
-// It takes two parameters, the domain to be queried and the keytag of the DNSKEY
-// It returns a DNSKEY object and an error object
-// If the error is set, either the key does not exist, or there was an error checking
-// If the error is unset, the DNSKEY will be found in the returned object
-// I'm debating switching to the method in dsExistsInRegistry as it allows the caller to
-// determine whether there was an actual error, or the key doesn't exist.
-func dnskeyExistsInDns(qname string, keytag uint16) (dns.DNSKEY, error) {
-	dnskeys, err := getDnskeyFromDns(qname)
-	var r dns.DNSKEY
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: error retrieving DNSKEY records for %s: %s\n", qname, err)
-		return r, err
-	} else {
-		for key := range dnskeys {
-			if key == keytag {
-				_debug(fmt.Sprintf("keytag %d exists with type %d\n", keytag, dnskeys[key].Flags))
-				return dnskeys[key], nil
-			}
-		}
-		return r, errors.New("key doesn't exist")
-	}
-}
-
-// _verbose takes a string and only outputs it if verbosity is requested via the -verbose CLI flag
-func _verbose(msgString string) {
-	if !*verbose {
-		return
-	}
-	fmt.Printf("%s\n", msgString)
-}
-
-// _debug takes a string and only outputs it if debug mode is enabled with the -debug CLI flag
-// function adds the calling function and line number to the output to aid debugging
-func _debug(debugString string) {
-	if !*debug {
-		return
-	}
-	pc, _, no, ok := runtime.Caller(1)
-	details := runtime.FuncForPC(pc)
-	if ok && details != nil {
-		fmt.Printf("DEBUG :: %s#%d :: %s\n", details.Name(), no, debugString)
-	} else {
-		fmt.Fprintf(os.Stderr, "Error: fatal error determining debug calling function")
-		os.Exit(1)
-	}
-}
-
-// parseConfigurationFile parses the configuration file
-// It takes one parameter, the config file. defaulting if -config is not passed on the CLI is handled in the flag parsing
-// It returns a config parser object and an array of errors
-// Mandatory items such as the API key and account number cause errors if missing
-// Items that can fall back to defaults, such as the nameserver, do so
-// Note that NOT passing an endpoint to the library causes it to default to production and so we don't need to
-func parseConfigurationFile(file string) (*configparser.ConfigParser, []error) {
-	_debug(fmt.Sprintf("loading configuration from %s", file))
-	p, err := configparser.NewConfigParserFromFile(file)
-	var errs []error
-	if err != nil {
-		errStr := fmt.Sprintf("error parsing config from %s: %s\n", file, err)
-		fmt.Fprint(os.Stderr, errStr)
-		errs = append(errs, errors.New(errStr))
-		return nil, errs
-	}
-
-	// mandatory settings
-	config["apiKey"], err = p.Get("api", "key")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: no API key in the config file\n")
-		errs = append(errs, errors.New("no API key in the config file"))
-	} else {
-		_verbose("API key set from configuration")
-	}
-	config["accountNumber"], err = p.Get("account", "number")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: no account number in the config file\n")
-		errs = append(errs, errors.New("no account number in the config file"))
-	} else {
-		_verbose(fmt.Sprintf("account number set to %s from configuration", config["accountNumber"]))
-	}
-
-	// optionals, with fallback defaults
-	config["nameserverAddr"], err = p.Get("nameserver", "address")
-	if err != nil || config["nameserverAddr"] == "" {
-		_verbose("no nameserver address in configuration; defaulting to 127.0.0.1")
-		config["nameserverAddr"] = "127.0.0.1"
-	} else {
-		_verbose(fmt.Sprintf("nameserver address set to %s from configuration", config["nameserverAddr"]))
-	}
-	config["nameserverPort"], err = p.Get("nameserver", "port")
-	if err != nil || config["nameserverPort"] == "" {
-		_verbose("no nameserver port in configuration; defaulting to 53")
-		config["nameserverPort"] = "53"
-	} else {
-		_verbose(fmt.Sprintf("nameserver port set to %s from configuration", config["nameserverPort"]))
-	}
-	config["dsDigestType"], err = p.Get("ds", "digest_type")
-	if err != nil || config["dsDigestType"] == "" {
-		_verbose("no DS record digest type in configuration; defaulting to 2 (SHA-256)")
-		config["dsDigestType"] = "2"
-	} else {
-		_verbose(fmt.Sprintf("DS records digest type set to %d from configuration", config["dsDigestType"]))
-	}
-	// optional, where fallback defaults are baked in
-	// for example, if you don't specify the endpoint, it'll default to prod
-	config["apiEndpoint"], err = p.Get("api", "endpoint")
-	if err != nil {
-		_debug("no API endpoint in configuration, so falling back to production")
-	}
-
-	if errs == nil {
-		return p, errs
-	} else {
-		return nil, errs
 	}
 }
